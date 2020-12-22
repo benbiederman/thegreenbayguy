726c40f26a06b0d96acb13caa55a21094992672b
benbiederman <benbiederman@gmail.com>
2020-12-22 15:08:49 -0600

    Local's Guide post updates
    Started each Local's Guide post

---
+++ js/popup.js
@@ -0,0 +1,22 @@
+const moreInfoBtn = document.querySelector('.open');
+const popup = document.querySelector('.lg-popup');
+const close = document.querySelector('.close');
+const popupBackground = document.querySelector('.popup');
+
+moreInfoBtn.addEventListener('click', e => {
+    e.preventDefault();
+    popup.style.display = 'block';
+    popup.style.position = 'sticky';
+    popupBackground.style.display = 'block';
+    popupBackground.style.position = 'sticky';
+    popup.style.animation = `popupFade 0.5s ease forwards`;
+    document.body.style.overflowY = 'hidden';
+})
+
+close.addEventListener('click', e => {
+    e.preventDefault();
+    popup.style.display = 'none';
+    popupBackground.style.display = 'none';
+    document.body.style.overflowY = 'auto';
+})
+
